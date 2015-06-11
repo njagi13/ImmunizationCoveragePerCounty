05ebb4ff7846613ec286306e707132b74b51e7c3
njagi13 <njagi13@gmail.com>
2015-06-11 21:03:29 +0300

    dlls for initial check out

---
+++ ImmunizationCoverage/ImmunizationCoverage/obj/Release/AspnetCompileMerge/TempBuildDir/App/ImmunizationCoverCtrl.js
@@ -0,0 +1,43 @@
+﻿app.controller('ImmunizationCoverCtrl', [
+    "$scope", "$http", "$timeout", function ($scope, $http, $timeout) {
+        $scope.datatodisplay = [];
+        $scope.message = "ImmunizationCoverCtrl";
+
+        $scope.counties = function () {
+            var url = '/api/immunizationcoverage/counties';
+            $http.get(url).then(function (res) {
+                debugger;
+                $scope.labels1 = res.data;
+                $scope.labels = $scope.labels1;
+                console.log($scope.alldata);
+            });
+        };
+        $scope.counties();
+
+
+        $scope.coverage = function () {
+            var url = '/api/immunizationcoverage/coverage';
+            $http.get(url).then(function (res) {
+                debugger;
+                $scope.cov = res.data;
+                $scope.data = $scope.cov;
+            });
+        };
+
+        $scope.coverage();
+
+
+      
+
+        $scope.series = ['2011', '2012', '2011'];
+
+        $scope.onClick = function (points, evt) {
+            console.log(points, evt);
+        };
+
+        // Simulate async data update
+        $timeout(function () {
+            $scope.data;
+        }, 3000);
+
+    }]);
